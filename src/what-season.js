7724c8053e9ceeaff26030630061d3ffdb6f20f7
Artem <ignatev.artem.nik@gmail.com>
2021-03-24 23:29:47 +0400

    init

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let seasons = {
+    winter: [0, 1, 11],
+    spring: [2, 3, 4],
+    summer: [5, 6, 7],
+    autumn: [8, 9, 10],
+  }; //d -date
+  if (Object.prototype.toString.call(d) === "[object Date]") {
+
+    
+    for (const [key, value] of Object.entries(seasons)) {
+      if (value.includes(d.getMonth())) {
+        return key;
+      }
+
+
+    }
+  }
+
+  else if (typeof date == 'object') {
+    throw 'exc';
+  }
+
+  else {
+    return 'Невозможно определить время года!';
+  }
 };
+
